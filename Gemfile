f57e66345889788ef1e99bb7710d54ba1501dd18
Charles Kubiak <charleskubiak@Charless-MacBook-Pro.local>
2020-09-06 12:48:12 -0500

    added categories

--- Gemfile
+++ Gemfile
@@ -60,6 +60,7 @@ end
 # Windows does not include zoneinfo files, so bundle the tzinfo-data gem
 gem "tzinfo-data", platforms: %i[mingw mswin x64_mingw jruby]
 
+gem "hirb"
 gem "rubocop"
 gem "rubocop-rails"
 gem "will_paginate", "~>3.1.0"
--- app/controllers/categories_controller.rb
+++ app/controllers/categories_controller.rb
@@ -1,6 +1,7 @@
 # frozen_string_literal: true
 
 class CategoriesController < ApplicationController
+  before_action :require_admin, except: %i[index show]
   def new
     @category = Category.new
   end
@@ -27,5 +28,12 @@ class CategoriesController < ApplicationController
 
   def category_params
     params.require(:category).permit(:name)
+  end
+
+  def require_admin
+    unless logged_in? && current_user.admin?
+      flash[:alert] = "Only admins can perform that action"
+      redirect_to categories_path
+    end
   end
 end
